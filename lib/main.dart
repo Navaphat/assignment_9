72cd82488a63eda03b7d8790ef294080a612dca0
Navaphat Phiboon <phiboon_n@silpakorn.edu>
2022-01-28 10:37:22 +0700

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class HomePage extends StatelessWidget {
         children: [
           Expanded(
             child: Container(
+              //color: Colors.red,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -49,73 +50,72 @@ class HomePage extends StatelessWidget {
             ),
           ),
 
-          Padding(
-            padding: const EdgeInsets.only(bottom: 20.0),
-            child: Expanded(
-              child: Container(
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        for(var i = 1; i <= 3; i++) buildButton(num: i),
-                      ],
-                    ),
+          Expanded(
+            child: Container(
+              //color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      for(var i = 1; i <= 3; i++) buildButton4(num: i),
+                    ],
+                  ),
 
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        for(var i = 4; i <= 6; i++) buildButton(num: i),
-                      ],
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      for(var i = 4; i <= 6; i++) buildButton4(num: i),
+                    ],
+                  ),
 
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        for(var i = 7; i <= 9; i++) buildButton(num: i),
-                      ],
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      for(var i = 7; i <= 9; i++) buildButton4(num: i),
+                    ],
+                  ),
 
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(width: 75.0,height: 75.0,),
-                        ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(width: 75.0,height: 75.0,),
+                      ),
 
-                        buildButton(num: 0),
+                      buildButton4(num: 0),
 
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            width: 75.0,
-                            height: 75.0,
-                            child: IconButton(
-                              onPressed: () {},
-                              icon: Icon(Icons.backspace_outlined, color: Colors.grey,),
-                              iconSize: 40.0,
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 75.0,
+                          height: 75.0,
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.backspace_outlined, color: Colors.grey,),
+                            iconSize: 40.0,
 
-                            ),
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
+                  ),
 
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8.0),
-                          child: TextButton(
-                              onPressed: () {},
-                              child: Text('ลืมรหัสผ่าน', style: TextStyle(fontSize: 20.0, color: Colors.blue),),
-                          ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: TextButton(
+                            onPressed: () {},
+                            child: Text('ลืมรหัสผ่าน', style: TextStyle(fontSize: 20.0, color: Colors.blue),),
                         ),
-                      ],
-                    )
-                  ],
-                ),
+                      ),
+                    ],
+                  )
+                ],
               ),
             ),
           )
@@ -125,7 +125,22 @@ class HomePage extends StatelessWidget {
   }
 }
 
-Widget buildButton({int? num}) {
+// Widget buildButton({int? num}) {
+//   return Padding(
+//     padding: const EdgeInsets.all(8.0),
+//     child: Container(
+//       width: 75.0,
+//       height: 75.0,
+//       child: OutlineButton(
+//         onPressed: () {},
+//         //shape: ,
+//         child: Text('${num.toString()}', style: TextStyle(fontSize: 25.0),),
+//       ),
+//     ),
+//   );
+// }
+
+Widget buildButton4({int? num}) {
   return Padding(
     padding: const EdgeInsets.all(8.0),
     child: Container(
@@ -134,7 +149,21 @@ Widget buildButton({int? num}) {
       child: OutlineButton(
         onPressed: () {},
         shape: CircleBorder(),
-        child: Text('${num.toString()}', style: TextStyle(fontSize: 25.0),),
+        child: Text('${num.toString()}', style: TextStyle(fontSize:25.0),),
+      ),
+      //color: Colors.white, // ห้ามกำหนด color ตรงนี้ ถ้าหากกำหนดใน BoxDecoration แล้ว
+      decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+          border: Border.all(color: Colors.grey, width: 2.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2), // สีเงา
+              offset: Offset(2, 4), // ทิศทางของเงาในแนวนอนและแนวตั้ง ตามลำดับ
+              blurRadius: 4.0,
+              spreadRadius: 2.0,
+            )
+          ]
       ),
     ),
   );
